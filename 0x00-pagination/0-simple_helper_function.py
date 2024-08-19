99d0dc215706a7bdf8e7076d681b13f56efd6b9d
jawkat <kad.joliano@gmail.com>
2024-08-19 11:29:42 +0100

    add comment and pycodestyle

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,7 +3,17 @@
 Main file
 """
 
+
 def index_range(page: int, page_size: int) -> tuple:
+    """_summary_
+
+    Args:
+        page (int): _description_
+        page_size (int): _description_
+
+    Returns:
+        tuple: _description_
+    """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
