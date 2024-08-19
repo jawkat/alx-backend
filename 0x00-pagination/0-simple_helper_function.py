5491d5957b8d7227b98796e2d4667313458f48db
jawkat <kad.joliano@gmail.com>
2024-08-19 11:28:30 +0100

    fisrt task

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+
+def index_range(page: int, page_size: int) -> tuple:
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
