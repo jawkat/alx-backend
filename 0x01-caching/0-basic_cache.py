4e8f0f74d0ec4af98303a54f02cb10e8f35d150f
jawkat <kad.joliano@gmail.com>
2024-08-22 01:02:13 +0100

    try

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,37 +1,35 @@
 #!/usr/bin/env python3
-"""A script for FIFO caching system
+"""A script for caching system
 """
 
 
-from collections import deque
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class FIFOCache(BaseCaching):
-    """FIFO cache system"""
+class BasicCache(BaseCaching):
+    """inherits from BaseCaching
 
+    Args:
+        BaseCaching (_type_): _description_
+    """
     def __init__(self):
+        """initialize BasicCache"""
         super().__init__()
-        self.order = deque()
 
     def put(self, key, item):
-        """ put key value pair into cache"""
-        if (key is None or item is None):
+        """assign to the dictionary
+        self.cache_data the item value for the key key
+        """
+        if key is None or item is None:
             return
-        capacity = BaseCaching.MAX_ITEMS
         self.cache_data[key] = item
-        self.order.append(key)
-        if key in self.cache_data:
-            self.cache_data[key] = item
-        if len(self.cache_data) >= capacity:
-            key_to_pop = self.order.popleft()
-            del self.cache_data[key_to_pop]
-            print(f"DISCARD: {key_to_pop}")
 
     def get(self, key):
-        """ return the value in self.cache_data linked to key.
-        If key is None or if the key doesn’t exist
-        in self.cache_data, return None."""
-        if key is None or key not in self.cache_data:
+        """ return the value in self.cache_data linked to key
+
+        Args:
+            key (_type_): _description_
+        """
+        if key not in self.cache_data or key is None:
             return None
         return self.cache_data.get(key)
