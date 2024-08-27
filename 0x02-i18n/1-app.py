bf07f4180d688bd4035ec202e7f92ad2b0f53154
jawkat <kad.joliano@gmail.com>
2024-08-27 22:33:37 +0100

    sacond task

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" comments """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+class Config:
+    """_summary_
+    """
+    LANGUAGES = ["en","fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object(Config)
+
+@app.route("/")
+def hello():
+    """_summary_
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
