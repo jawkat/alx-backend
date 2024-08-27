de615ca648e51922b8a7015a6998162e8c071baf
jawkat <kad.joliano@gmail.com>
2024-08-27 23:59:38 +0100

    try

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,28 +1,33 @@
 #!/usr/bin/env python3
-""" comments """
-from flask import Flask, render_template, request
+"""
+Flask app
+"""
+from flask import (
+    Flask,
+    render_template,
+    request
+)
 from flask_babel import Babel
 
+
+class Config(object):
+    """
+    Configuration for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
 app = Flask(__name__)
+app.config.from_object(Config)
 babel = Babel(app)
 
 
-class Config:
-    """_summary_
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-app.config.from_object(Config)
-
 @babel.localeselector
 def get_locale():
-    """_summary_
-
-    Returns:
-        _type_: _description_
+    """
+    Select and return best language match based on supported languages
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
