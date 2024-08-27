d1f6e07e59585d9204911785175fcaf3ec0ce31b
jawkat <kad.joliano@gmail.com>
2024-08-27 22:12:38 +0100

    firest task

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,14 @@
+""" comments """
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    """_summary_
+    """
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
