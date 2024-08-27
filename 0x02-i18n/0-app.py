da244fa9d2d7f252c97b4b9f2c22db3d8ab90d31
jawkat <kad.joliano@gmail.com>
2024-08-27 22:20:29 +0100

    enlev resposnse

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """ comments """
 from flask import Flask, render_template
-from flask.wrappers import Response
+
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello() -> Response:
+def hello():
     """_summary_
     """
     return render_template('0-index.html')
